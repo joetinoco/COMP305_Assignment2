43ec69db20717621d84a10f10296b780af340436
joetinoco <jose.tinoco@gmail.com>
2016-10-19 20:12:00 -0400

    Enemy refactor

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -1,54 +1,85 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+/*
+
+ENEMY CONTROLLER
+================
+
+Manage enemy movement and death.
+
+*/
+
 public class EnemyController : MonoBehaviour {
 
-	private Transform _transform;
-	
-	public float Speed = 2f;
-	public int enemyPoints = 200;
-	public Transform MovementStart;
-	public Transform MovementEnd;
-	private bool reverseMovement = false;
-	private GameObject gameController;
+	// ==========================================
+	// Attributes
+	// ==========================================
 
-	// Use this for initialization
+	private Transform eTransform;
+	private GameObject gameController;
+	
+	// Enemy attributes
+	public float speed = 2f;
+	public int enemyPoints = 200;
+	
+	// Movement follows the same rationale of moving platforms,
+	// each enemy instance has those two points set in Unity GUI for easy reuse. 
+	public Transform movementStart;
+	public Transform movementEnd;
+	private bool reverseMovement = false;
+	
+
+	// ==========================================
+	// Object initialization
+	// ==========================================
 	void Start () {
-		this._transform = GetComponent<Transform>();
+		this.eTransform = GetComponent<Transform>();
 		gameController = GameObject.Find("GameController");
 	}
 	
-	// Update is called once per frame
+	// ==========================================
+	// Game Lifecycle Updates
+	// ==========================================
 	void FixedUpdate () {
 		
-		if (!this.MovementStart || !this.MovementEnd) return;
+		// Prevent movement (and runtime errors)
+		// if the enemy has no waypoints set
+		if (!this.movementStart || !this.movementEnd) return;
 		
-		Debug.DrawLine(this.MovementStart.position, 
-				this.MovementEnd.position);
+		Debug.DrawLine(this.movementStart.position, 
+				this.movementEnd.position);
 		
+		// Select a starting (or ending) point to 
+		// calculate movement towards.
 		Vector2 movementDestination;
 		if (reverseMovement == false) {
 			movementDestination = new Vector2(
-				this.MovementEnd.position.x,
-				this.MovementEnd.position.y);
+				this.movementEnd.position.x,
+				this.movementEnd.position.y);
 		} else {
 			movementDestination = new Vector2(
-				this.MovementStart.position.x,
-				this.MovementStart.position.y);
+				this.movementStart.position.x,
+				this.movementStart.position.y);
 		}
-
-		this._transform.position = Vector2.MoveTowards(
-			new Vector2(this._transform.position.x, this._transform.position.y),
+		
+		// Move the enemy towards it
+		this.eTransform.position = Vector2.MoveTowards(
+			new Vector2(this.eTransform.position.x, this.eTransform.position.y),
 			movementDestination,
-			this.Speed * Time.deltaTime
+			this.speed * Time.deltaTime
 		);
 
-		if (this._transform.position.x == movementDestination.x && this._transform.position.y == movementDestination.y) {
+		// Flip the start/end waypoints if the enemy reaches either
+		if (this.eTransform.position.x == movementDestination.x && this.eTransform.position.y == movementDestination.y) {
 			reverseMovement = !reverseMovement;
 		}
 
 	}
 
+	// Collision detection methods
+	// =================================================
+	
 	private void OnCollisionEnter2D(Collision2D other) {
 		if (other.gameObject.CompareTag("Bullet")) {
 			other.gameObject.SendMessage("DestroyBullet");
